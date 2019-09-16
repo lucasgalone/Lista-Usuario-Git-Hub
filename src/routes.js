a6b2fb7784b897fd0854d7f4344eb18e04a1c573
Lucas Vinicius Galone <lucas.vinicius.galone@hotmail.com>
2019-09-16 01:11:23 -0300

    Sobe projeto para o GitHub

---
+++ src/routes.js
@@ -0,0 +1,35 @@
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+
+import Main from './pages/Main';
+import User from './pages/User';
+
+const Routes = createAppContainer(
+  createStackNavigator(
+    {
+      Main,
+      User,
+      // Main: {
+      //   screen: Main,
+      //   navigationOptions: () => ({
+      //     title: 'Usuários',
+      //   }),
+      // },
+      // User: {
+      //   navigationOptions: () => ({}),
+      // },
+    },
+    {
+      headerLayoutPreset: 'center',
+      headerBackTitleVisible: false,
+      defaultNavigationOptions: {
+        headerStyle: {
+          backgroundColor: '#7159c1',
+        },
+        headerTintColor: '#FFF',
+      },
+    }
+  )
+);
+
+export default Routes;
